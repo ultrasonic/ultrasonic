b57a425e9da99e237c791abb487cf142ef2b9f64
Yahor Berdnikau <egorr.berd@gmail.com>
2017-08-12 20:19:27 +0200

    Implement converting Indexes Subsonic api entity to domain entity.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- ultrasonic/src/main/kotlin/org/moire/ultrasonic/data/SubsonicAPIConverter.kt
+++ ultrasonic/src/main/kotlin/org/moire/ultrasonic/data/SubsonicAPIConverter.kt
@@ -1,8 +1,13 @@
-// Converts entities from [org.moire.ultrasonic.api.subsonic.SubsonicAPIClient] to app entities.
+// Converts entities from [org.moire.ultrasonic.api.subsonic.SubsonicAPIClient] to app domain entities.
 @file:JvmName("APIConverter")
 package org.moire.ultrasonic.data
 
+import org.moire.ultrasonic.api.subsonic.models.Index
+import org.moire.ultrasonic.domain.Artist
+import org.moire.ultrasonic.domain.Indexes
 import org.moire.ultrasonic.domain.MusicFolder
+import org.moire.ultrasonic.api.subsonic.models.Artist as APIArtist
+import org.moire.ultrasonic.api.subsonic.models.Indexes as APIIndexes
 import org.moire.ultrasonic.api.subsonic.models.MusicFolder as APIMusicFolder
 
 fun APIMusicFolder.toDomainEntity(): MusicFolder = MusicFolder(this.id.toString(), this.name)
@@ -10,6 +15,14 @@ fun APIMusicFolder.toDomainEntity(): MusicFolder = MusicFolder(this.id.toString(
 fun List<APIMusicFolder>.toDomainEntityList(): List<MusicFolder>
         = this.map { it.toDomainEntity() }
 
-fun convertMusicFolderList(entitiesList: List<APIMusicFolder>): List<MusicFolder> {
-    return entitiesList.map { convertMusicFolder(it) }
+fun APIIndexes.toDomainEntity(): Indexes = Indexes(this.lastModified, this.ignoredArticles,
+        this.shortcuts.foldIndexToArtistList(), this.indexList.foldIndexToArtistList())
+
+private fun List<Index>.foldIndexToArtistList(): List<Artist> = this.fold(listOf(), {
+    acc, index -> acc + index.artists.map { it.toDomainEntity() }
+})
+
+fun APIArtist.toDomainEntity(): Artist = Artist().apply {
+    id = this@toDomainEntity.id.toString()
+    name = this@toDomainEntity.name
 }
