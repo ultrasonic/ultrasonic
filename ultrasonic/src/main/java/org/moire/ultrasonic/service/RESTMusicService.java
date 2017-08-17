864488510c66a4a533f888f87578ed94f9bcf4cd
Yahor Berdnikau <egorr.berd@gmail.com>
2017-08-17 22:38:23 +0200

    Use new subsonic api star request.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- ultrasonic/src/main/java/org/moire/ultrasonic/service/RESTMusicService.java
+++ ultrasonic/src/main/java/org/moire/ultrasonic/service/RESTMusicService.java
@@ -317,42 +317,21 @@ public class RESTMusicService implements MusicService
         return String.format(Locale.US, "indexes-%d.ser", Math.abs(s.hashCode()));
     }
 
-	@Override
-	public void star(String id, String albumId, String artistId, Context context, ProgressListener progressListener) throws Exception
-	{
-		checkServerVersion(context, "1.8", "Starring not supported.");
+    @Override
+    public void star(String id,
+                     String albumId,
+                     String artistId,
+                     Context context,
+                     ProgressListener progressListener) throws Exception {
+        Long apiId = id == null ? null : Long.valueOf(id);
+        Long apiAlbumId = albumId == null ? null : Long.valueOf(albumId);
+        Long apiArtistId = artistId == null ? null : Long.valueOf(artistId);
 
-		List<String> parameterNames = new LinkedList<String>();
-		List<Object> parameterValues = new LinkedList<Object>();
-
-		if (id != null)
-		{
-			parameterNames.add("id");
-			parameterValues.add(id);
-		}
-
-		if (albumId != null)
-		{
-			parameterNames.add("albumId");
-			parameterValues.add(albumId);
-		}
-
-		if (artistId != null)
-		{
-			parameterNames.add("artistId");
-			parameterValues.add(artistId);
-		}
-
-		Reader reader = getReader(context, progressListener, "star", null, parameterNames, parameterValues);
-		try
-		{
-			new ErrorParser(context).parse(reader);
-		}
-		finally
-		{
-			Util.close(reader);
-		}
-	}
+        updateProgressListener(progressListener, R.string.parser_reading);
+        Response<SubsonicResponse> response = subsonicAPIClient.getApi()
+                .star(apiId, apiAlbumId, apiArtistId).execute();
+        checkResponseSuccessful(response);
+    }
 
 	@Override
 	public void unstar(String id, String albumId, String artistId, Context context, ProgressListener progressListener) throws Exception
