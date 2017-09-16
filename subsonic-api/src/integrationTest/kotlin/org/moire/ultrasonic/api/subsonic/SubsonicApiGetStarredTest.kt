bad5cf5a9a22b17d6acbb5014dd39b5d9ca20a2b
Yahor Berdnikau <egorr.berd@gmail.com>
2017-09-16 22:19:17 +0200

    Add getStarred call.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

---
+++ subsonic-api/src/integrationTest/kotlin/org/moire/ultrasonic/api/subsonic/SubsonicApiGetStarredTest.kt
@@ -0,0 +1,47 @@
+package org.moire.ultrasonic.api.subsonic
+
+import org.amshove.kluent.`should equal to`
+import org.amshove.kluent.`should equal`
+import org.junit.Test
+import org.moire.ultrasonic.api.subsonic.models.Artist
+import org.moire.ultrasonic.api.subsonic.models.SearchTwoResult
+
+/**
+ * Integration test for [SubsonicAPIClient] for getStarred call.
+ */
+class SubsonicApiGetStarredTest : SubsonicAPIClientTest() {
+    @Test
+    fun `Should handle error response`() {
+        val response = checkErrorCallParsed(mockWebServerRule) {
+            client.api.getStarred().execute()
+        }
+
+        response.starred `should equal` SearchTwoResult()
+    }
+
+    @Test
+    fun `Should handle ok reponse`() {
+        mockWebServerRule.enqueueResponse("get_starred_ok.json")
+
+        val response = client.api.getStarred().execute()
+
+        assertResponseSuccessful(response)
+        with(response.body().starred) {
+            albumList `should equal` emptyList()
+            artistList.size `should equal to` 1
+            artistList[0] `should equal` Artist(id = 364, name = "Parov Stelar",
+                    starred = parseDate("2017-08-12T18:32:58.768Z"))
+            songList `should equal` emptyList()
+        }
+    }
+
+    @Test
+    fun `Should pass music folder id in request param`() {
+        val musicFolderId = 441L
+
+        mockWebServerRule.assertRequestParam(responseResourceName = "get_starred_ok.json",
+                expectedParam = "musicFolderId=$musicFolderId") {
+            client.api.getStarred(musicFolderId = musicFolderId).execute()
+        }
+    }
+}
