3bbd1fb16b5b0b926d52dc5e44494ea9313fc31e
Yahor Berdnikau <egorr.berd@gmail.com>
2017-11-10 21:31:48 +0100

    Add methods to convert api share entity to domain entity.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- ultrasonic/src/main/java/org/moire/ultrasonic/domain/Share.java
+++ ultrasonic/src/main/java/org/moire/ultrasonic/domain/Share.java
@@ -7,8 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-public class Share implements Serializable
-{
+public class Share implements Serializable {
 	private static final long serialVersionUID = 1487561657691009668L;
 	private static final Pattern urlPattern = Pattern.compile(".*/([^/?]+).*");
 	private String id;
@@ -120,4 +119,40 @@ public class Share implements Serializable
 	{
 		entries.add(entry);
 	}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Share share = (Share) o;
+
+        if (id != null ? !id.equals(share.id) : share.id != null) return false;
+        if (url != null ? !url.equals(share.url) : share.url != null) return false;
+        if (description != null ? !description.equals(share.description) : share.description != null)
+            return false;
+        if (username != null ? !username.equals(share.username) : share.username != null)
+            return false;
+        if (created != null ? !created.equals(share.created) : share.created != null) return false;
+        if (lastVisited != null ? !lastVisited.equals(share.lastVisited) : share.lastVisited != null)
+            return false;
+        if (expires != null ? !expires.equals(share.expires) : share.expires != null) return false;
+        if (visitCount != null ? !visitCount.equals(share.visitCount) : share.visitCount != null)
+            return false;
+        return entries != null ? entries.equals(share.entries) : share.entries == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (url != null ? url.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (created != null ? created.hashCode() : 0);
+        result = 31 * result + (lastVisited != null ? lastVisited.hashCode() : 0);
+        result = 31 * result + (expires != null ? expires.hashCode() : 0);
+        result = 31 * result + (visitCount != null ? visitCount.hashCode() : 0);
+        result = 31 * result + (entries != null ? entries.hashCode() : 0);
+        return result;
+    }
 }
