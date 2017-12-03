336f505ba5afdd79d53247bab91649edf4f5fc82
Yahor Berdnikau <egorr.berd@gmail.com>
2017-12-03 14:14:01 +0100

    Replace ServerTooOldException with more proper ApiNotSupportedException.

---
+++ subsonic-api/src/main/kotlin/org/moire/ultrasonic/api/subsonic/ApiNotSupportedException.kt
@@ -0,0 +1,11 @@
+package org.moire.ultrasonic.api.subsonic
+
+import java.io.IOException
+
+/**
+ * Special [IOException] to indicate that called api is not yet supported
+ * by current server api version.
+ */
+class ApiNotSupportedException(
+        serverApiVersion: SubsonicAPIVersions)
+    : IOException("Server api $serverApiVersion does not support this call")
