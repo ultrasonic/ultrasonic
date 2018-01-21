7830ed3dbf9c8b636a813ff8c4793f5a2c9085d9
Yahor Berdnikau <egorr.berd@gmail.com>
2018-01-21 18:06:41 +0100

    Add flag to force using hex password authentication.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- subsonic-api/src/main/kotlin/org/moire/ultrasonic/api/subsonic/interceptors/ProxyPasswordInterceptor.kt
+++ subsonic-api/src/main/kotlin/org/moire/ultrasonic/api/subsonic/interceptors/ProxyPasswordInterceptor.kt
@@ -7,15 +7,21 @@ import org.moire.ultrasonic.api.subsonic.SubsonicAPIVersions
 
 /**
  * Proxy [Interceptor] that uses one of [hexInterceptor] or [mD5Interceptor] depends on [apiVersion].
+ *
+ * To force [hexInterceptor] set [forceHexPassword] to `true`. Usually it should be done only for
+ * ldap users.
  */
 internal class ProxyPasswordInterceptor(
         initialAPIVersions: SubsonicAPIVersions,
         private val hexInterceptor: PasswordHexInterceptor,
-        private val mD5Interceptor: PasswordMD5Interceptor) : Interceptor {
+        private val mD5Interceptor: PasswordMD5Interceptor,
+        private val forceHexPassword: Boolean = false
+) : Interceptor {
     var apiVersion: SubsonicAPIVersions = initialAPIVersions
 
     override fun intercept(chain: Chain): Response =
-            if (apiVersion < SubsonicAPIVersions.V1_13_0) {
+            if (apiVersion < SubsonicAPIVersions.V1_13_0 ||
+                    forceHexPassword) {
                 hexInterceptor.intercept(chain)
             } else {
                 mD5Interceptor.intercept(chain)
