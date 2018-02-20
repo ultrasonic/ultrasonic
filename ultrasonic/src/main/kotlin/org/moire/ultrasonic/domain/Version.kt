4218698a16b0b4e95e9ff9d136d868b2fe5ecaa6
Yahor Berdnikau <egorr.berd@gmail.com>
2018-02-20 21:54:53 +0100

    Convert Version class to Kotlin.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

---
+++ ultrasonic/src/main/kotlin/org/moire/ultrasonic/domain/Version.kt
@@ -0,0 +1,27 @@
+package org.moire.ultrasonic.domain
+
+import net.swiftzer.semver.SemVer
+
+/**
+ * Represents the version number of the Subsonic Android app.
+ */
+data class Version(
+        val version: SemVer
+) : Comparable<Version> {
+
+    override fun compareTo(other: Version): Int {
+        return version.compareTo(other.version)
+    }
+
+    companion object {
+        /**
+         * Creates a new version instance by parsing the given string.
+         *
+         * @param version A string of the format "1.27", "1.27.2" or "1.27.beta3".
+         */
+        @JvmStatic
+        fun fromCharSequence(version: String): Version {
+            return Version(SemVer.parse(version))
+        }
+    }
+}
