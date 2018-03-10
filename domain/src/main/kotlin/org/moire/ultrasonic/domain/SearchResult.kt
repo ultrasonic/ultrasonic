ad52e3ad95c3a9a2eb5179c10e74ab8a8656fb35
Yahor Berdnikau <egorr.berd@gmail.com>
2018-03-10 18:24:37 +0100

    Move domain entities to own module.
    This allow other modules to depend on them.
    
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- domain/src/main/kotlin/org/moire/ultrasonic/domain/SearchResult.kt
+++ domain/src/main/kotlin/org/moire/ultrasonic/domain/SearchResult.kt
@@ -1,10 +1,12 @@
 package org.moire.ultrasonic.domain
 
+import org.moire.ultrasonic.domain.MusicDirectory.Entry
+
 /**
  * The result of a search.  Contains matching artists, albums and songs.
  */
 data class SearchResult(
         val artists: List<Artist>,
-        val albums: List<MusicDirectory.Entry>,
-        val songs: List<MusicDirectory.Entry>
+        val albums: List<Entry>,
+        val songs: List<Entry>
 )
