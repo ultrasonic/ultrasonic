c2924df25f417692994bbfbf5d01a9e1f1891b9a
Yahor Berdnikau <egorr.berd@gmail.com>
2018-08-03 22:08:30 +0200

    Use Koin java module.
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- ultrasonic/src/main/kotlin/org/moire/ultrasonic/app/UApp.kt
+++ ultrasonic/src/main/kotlin/org/moire/ultrasonic/app/UApp.kt
@@ -1,7 +1,6 @@
 package org.moire.ultrasonic.app
 
 import android.app.Application
-import org.koin.android.ext.android.get
 import org.koin.android.ext.android.startKoin
 import org.moire.ultrasonic.di.DiProperties
 import org.moire.ultrasonic.di.appPermanentStorage
@@ -9,8 +8,6 @@ import org.moire.ultrasonic.di.baseNetworkModule
 import org.moire.ultrasonic.di.directoriesModule
 import org.moire.ultrasonic.di.featureFlagsModule
 import org.moire.ultrasonic.di.musicServiceModule
-import org.moire.ultrasonic.featureflags.FeatureStorage
-import org.moire.ultrasonic.subsonic.loader.image.SubsonicImageLoader
 
 class UApp : Application() {
     override fun onCreate() {
@@ -28,19 +25,5 @@ class UApp : Application() {
                 DiProperties.APP_CONTEXT to applicationContext
             )
         )
-    }
-
-    /**
-     * Temporary method to get subsonic image loader from java code.
-     */
-    fun getSubsonicImageLoader(): SubsonicImageLoader {
-        return get()
-    }
-
-    /**
-     * Temporary method to get features storage.
-     */
-    fun getFeaturesStorage(): FeatureStorage {
-        return get()
     }
 }
