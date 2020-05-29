915a659b5d4c57e2d496aa908c3941c2ca16fe73
Nite <shunite@gmail.com>
2020-05-29 11:23:51 +0200

    Fixed MultiDex

--- ultrasonic/src/main/kotlin/org/moire/ultrasonic/app/UApp.kt
+++ ultrasonic/src/main/kotlin/org/moire/ultrasonic/app/UApp.kt
@@ -1,6 +1,7 @@
 package org.moire.ultrasonic.app
 
 import android.app.Application
+import androidx.multidex.MultiDexApplication
 import org.koin.android.ext.android.startKoin
 import org.moire.ultrasonic.di.DiProperties
 import org.moire.ultrasonic.di.appPermanentStorage
@@ -9,7 +10,7 @@ import org.moire.ultrasonic.di.directoriesModule
 import org.moire.ultrasonic.di.featureFlagsModule
 import org.moire.ultrasonic.di.musicServiceModule
 
-class UApp : Application() {
+class UApp : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
 
