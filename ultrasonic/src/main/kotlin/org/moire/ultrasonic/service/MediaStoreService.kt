b438559da60ff5d93bd3066766d03fabc495163c
tzugen <tzugen@riseup.net>
2021-04-16 11:47:16 +0200

    Update license of MediaStoreService.kt

--- ultrasonic/src/main/kotlin/org/moire/ultrasonic/service/MediaStoreService.kt
+++ ultrasonic/src/main/kotlin/org/moire/ultrasonic/service/MediaStoreService.kt
@@ -1,21 +1,10 @@
 /*
- This file is part of Subsonic.
-
- Subsonic is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- Subsonic is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with Subsonic.  If not, see <http://www.gnu.org/licenses/>.
-
- Copyright 2009 (C) Sindre Mehus
+ * MediaStoreService.kt
+ * Copyright (C) 2009-2021 Ultrasonic developers
+ *
+ * Distributed under terms of the GNU GPLv3 license.
  */
+
 package org.moire.ultrasonic.service
 
 import android.content.ContentValues
