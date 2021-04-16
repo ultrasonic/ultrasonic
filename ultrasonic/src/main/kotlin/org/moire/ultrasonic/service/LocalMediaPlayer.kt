0efb3547c30de38714955a9a54c20229c527e3bf
tzugen <tzugen@riseup.net>
2021-04-16 11:45:37 +0200

    Update license of two files.

--- ultrasonic/src/main/kotlin/org/moire/ultrasonic/service/LocalMediaPlayer.kt
+++ ultrasonic/src/main/kotlin/org/moire/ultrasonic/service/LocalMediaPlayer.kt
@@ -1,3 +1,10 @@
+/*
+ * LocalMediaPlayer.kt
+ * Copyright (C) 2009-2021 Ultrasonic developers
+ *
+ * Distributed under terms of the GNU GPLv3 license.
+ */
+
 package org.moire.ultrasonic.service
 
 import android.app.PendingIntent
