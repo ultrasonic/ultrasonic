956d555b3503419fbe915fbdd4cf569f38e37b04
tzugen <67737443+tzugen@users.noreply.github.com>
2021-06-19 23:23:39 +0200

    Update PlayerFragment.java

--- ultrasonic/src/main/java/org/moire/ultrasonic/fragment/PlayerFragment.java
+++ ultrasonic/src/main/java/org/moire/ultrasonic/fragment/PlayerFragment.java
@@ -992,7 +992,6 @@ public class PlayerFragment extends Fragment implements GestureDetector.OnGestur
                 currentSong.setStarred(true);
             }
 
-            // Code is duplicated with MediaPlayerController:457 FIXME: There should be a better way
             new Thread(new Runnable() {
                 @Override
                 public void run() {
