53ae0cd23293335f7fb186b9e48c694bc302a897
tzugen <tzugen@riseup.net>
2022-05-31 10:18:23 +0200

    Update copyright

--- ultrasonic/src/main/kotlin/org/moire/ultrasonic/playback/PlaybackService.kt
+++ ultrasonic/src/main/kotlin/org/moire/ultrasonic/playback/PlaybackService.kt
@@ -1,17 +1,8 @@
 /*
- * Copyright 2021 The Android Open Source Project
+ * PlaybackService.kt
+ * Copyright (C) 2009-2022 Ultrasonic developers
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Distributed under terms of the GNU GPLv3 license.
  */
 package org.moire.ultrasonic.playback
 
@@ -71,6 +62,7 @@ class PlaybackService : MediaLibraryService(), KoinComponent {
     }
 
     override fun onCreate() {
+        Timber.i("onCreate called")
         super.onCreate()
         initializeSessionAndPlayer()
     }
